b97771353e45b893896ad6e29710b58eea83d82b
richard ingwe <richardingwe5@gmail.com>
2021-12-29 08:34:01 +0100

    showed a text widget inside the floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('hi there');
+        },
+        child: Text(
+          '+',
+          style: TextStyle(
+            fontSize: 32.0,
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Text("Let's see some images"),
       ),
