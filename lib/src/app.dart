e29102147460bf7a894a4bb15fb28445cc3abd25
richard ingwe <richardingwe5@gmail.com>
2021-12-29 16:09:30 +0100

    added counter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -8,13 +8,22 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Center(
+          child: Text('$counter'),
+        ),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
               print('hi there');
+              setState(() {
+                counter++;
+              });
+              print(counter);
             },
             child: Icon(Icons.add)),
         appBar: AppBar(
