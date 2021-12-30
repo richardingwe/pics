a93e45c57c85314a99c08d753b81d3111a28c785
richard ingwe <richardingwe5@gmail.com>
2021-12-30 21:01:28 +0100

    done with image model

--- lib/src/models/image_model.dart
+++ lib/src/models/image_model.dart
@@ -2,4 +2,12 @@ class ImageModel {
   int id;
   String url;
   String title;
+
+  ImageModel(this.id, this.url, this.title);
+
+  ImageModel.fromJson(parsedJson) {
+    id = parsedJson['id'];
+    url = parsedJson['url'];
+    title = parsedJson['title'];
+  }
 }
